e456adcc549ce956feeb975f8d58cf046bdfd158
dattasai <chinnidattasai@gmail.com>
2025-05-06 14:05:40 +0530

    feat: added updation of student

--- applot/views.py
+++ applot/views.py
@@ -31,6 +31,17 @@ def get_student(request, pk):
     serializer = StudentSerializer(student)
     return Response(serializer.data)
 
+@api_view(['PUT'])
+def update_student(request, pk):
+    try:
+        student = Student.objects.get(pk=pk)
+    except Student.DoesNotExist:
+        return Response({'error': 'Student not found'}, status=status.HTTP_404_NOT_FOUND)
+    serializer = StudentSerializer(student, data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 @api_view(['DELETE'])
 def delete_student(request, pk):
